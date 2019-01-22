42ccd6c465e130d2c7c826c18a93f4a2c590f275
Thanamongkhon Yamdej <tyamdej@gmail.com>
2019-01-22 21:51:07 +0700

    First time

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    client: './src/client.js',
+    bundle: './src/bundle.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+ }
+
+}
